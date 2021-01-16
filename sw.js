bf96727c8a7dcf1921e3d73fc307e46bb2611803
zehengl <imzehengl@gmail.com>
2021-01-16 22:48:57 +0000

    Deploying to master from  @ f51bffd6325cdc242d616b7861c4a00bd94a5a28 🚀

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8da996fa85cc626ef602cde945f9cfe7"
+    "revision": "08957c72678bc61ad669fef3ac942b60"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
