fe382be7c5b80c17492f634c666395688ec75ba4
zehengl <imzehengl@gmail.com>
2021-05-22 20:59:18 +0000

    Deploying to master from  @ 8ba9d321ca68729e9ebdc73846174ed392747cc1 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3ca95c91635c360fd3a6.js"
+    "url": "webpack-runtime-c4ca9a3904619293e7c9.js"
   },
   {
     "url": "framework-5c2ca0f07ab11edb5b82.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "de47a0e139052f0e501dc6ce4647bab2"
+    "revision": "2c773dd8ffd7978f163c0574214bb308"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-f69451057ef7bd5e6f8e.js"
