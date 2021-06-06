bd5b5436965e2258a0df76e02736e25970d2c9e6
Zeheng Li <imzehengl@gmail.com>
2021-06-06 15:10:37 -0600

    use function component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,19 @@
+import Layout from "../components/layout"
 import React from "react"
+import SEO from "../components/seo"
 import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+function NotFoundPage(props) {
+  const location = props.location
+  const siteTitle = props.data.site.siteMetadata.title
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
-    )
-  }
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="404: Not Found" />
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  )
 }
 
 export default NotFoundPage
