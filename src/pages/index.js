bcb51d1e20ac9b8cf4e642c7b12087a05d23ac88
Zeheng Li <imzehengl@gmail.com>
2023-05-17 01:35:28 -0600

    update queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,7 +54,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
       edges {
         node {
           excerpt
